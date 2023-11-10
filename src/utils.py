d64415436ee114efaef93066204992d8834287aa
CHANDAN PRAKASH <chandannadve@gmail.com>
2023-11-10 15:22:32 +0400

    Pipeline and Webapplication

--- src/utils.py
+++ src/utils.py
@@ -18,4 +18,49 @@ def save_object(file_path, obj):
             pickle.dump(obj, file_obj)
 
     except Exception as e:
+        logging.info('Exception occured in save_object function utils')
         raise CustomException(e, sys)
+
+import numpy as np
+
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            # Train model
+            model.fit(X_train,y_train)
+
+            
+
+            # Predict Testing data
+            y_test_pred =model.predict(X_test)
+
+            # Get R2 scores for train and test data
+            #train_model_score = r2_score(ytrain,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] =  test_model_score
+
+        return report
+
+    except Exception as e:
+        logging.info('Exception occured during model training')
+        raise CustomException(e,sys)
+  
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        logging.info('Exception occured in load_object function utils')
+        raise CustomException(e, sys)
+    
+    '''
+    mae=mean_absolute_error(true,predicted)
+    mse=mean_squared_error(true,predicted)
+    rmse=np.sqrt(mse)
+    r2_square=r2_score(true,predicted)
+    return mae,rmse,r2_square
+    '''
